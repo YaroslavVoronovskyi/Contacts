b81791de9982185d09accbf090f7c3ab855ca2c5
yvoronovskyi <yaroslav.voronovskyi@gmail.com>
2022-09-08 06:25:54 +0300

    Deleted extra method.

--- src/contacts/service/Validator.java
+++ src/contacts/service/Validator.java
@@ -5,20 +5,6 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-//    public static boolean validateValueByPattern(String value, Pattern pattern) {
-////        boolean isValueValid = false;
-////        while (!isValueValid) {
-//////            value = ConsoleReader.getStringFromConsole(message);
-////            isValueValid = validateFieldValue(value, pattern);
-////            if (!isValueValid) {
-////                System.out.println(errorMessage);
-////            }
-////        }
-//
-//
-//        return validateFieldValue(value, pattern);
-//    }
-
     public static boolean validateFieldValue(String value, Pattern pattern) {
         Matcher matcher = pattern.matcher(value);
         return matcher.matches();
